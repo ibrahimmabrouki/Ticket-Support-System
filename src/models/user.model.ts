e3e68ca04c21e2af6fa39749f449b325c6123e60
Ibrahim Mabrouki <ibrahim.mabrouki@lau.edu>
2026-02-09 14:22:06 +0200

    update model(user)

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,16 +1,25 @@
 import { Schema, model, Document } from "mongoose";
 
 export interface IUser extends Document {
-    name: string;
-    email: string;
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
 }
 
 const userSchema = new Schema<IUser>(
-    {
-        name: { type: String, required: true, trim: true },
-        email: { type: String, required: true, unique: true, lowercase: true }
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: {
+      type: String,
+      required: true,
+      lowercase: true,
+      minlength: 6,
     },
-    { timestamps: true }
+    phone: { type: String, required: true },
+  },
+  { timestamps: true }
 );
 
 export default model<IUser>("users", userSchema);
