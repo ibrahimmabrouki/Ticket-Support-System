ebcd7696fd1ef7cf63cf116634d4bd03ef9d2e0d
yahyafl <yahyamano48@gmail.com>
2026-02-11 22:51:11 +0200

    fixing auth

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -16,7 +16,7 @@ const userSchema = new Schema<IUser>(
   {
     name: { type: String, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, lowercase: true, minlength: 6 },
+    password: { type: String, required: true, minlength: 6 },
     phone: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     role: { type: String, enum: ["employee", "client"], default: "employee", required: true },
